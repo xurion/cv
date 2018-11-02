27d366c2a8ed89f58f92b53a788f9b9f13070ca2
Dean James <xurion@hotmail.com>
2018-11-02 07:52:01 +0000

    Updated focus of profile to development.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
             <Col xs={6} className="d-sm-none offset-3">
               <Portrait/>
             </Col>
-            <div className="h2 text-center text-lg-left">Software Delivery &amp; Agile Solutions Professional</div>
+            <div className="h2 text-center text-lg-left">Front-end Software Engineer &amp; Agile Solutions Architect</div>
           </Col>
           <Col xs={6} sm={4} md={3} lg={2} className="d-none d-sm-block text-lg-right">
             <Portrait/>
@@ -38,13 +38,36 @@ export default class App extends Component {
         <Row>
           <Col xs={12} lg={6}>
             <Subtitle>Introduction</Subtitle>
-            <p>Hi! I'm Dean. I'm a confident, highly motivated and passionate Agile professional with a broad range of skills. I have
-              more than six years experience in nurturing teams to focus and deliver customer and business needs. I
-              pride myself on enabling and guiding evolution at the team and organisation level, consistently striving
-              for a high standard of work, being good-natured, outgoing and having a good sense of humour.</p>
+            <p>Hi! I'm Dean. I'm a passionate, customer-focused and quality-first front-end software engineer with a
+              broad range of skills. I have more than ten years experience in delivering business and customer needs in
+              an Agile way, as well as nurturing teams to become the best delivery-focused units they can. I pride
+              myself on enabling and guiding evolution at the team level, consistently striving for a high standard of
+              work, being good-natured, outgoing and having a good sense of humour.</p>
             <Br/>
 
-            <Subtitle>Skills</Subtitle>
+            <Subtitle>Dev skills</Subtitle>
+            <List points={[
+              'JavaScript, ES6, JSX',
+              'React, Redux',
+              'jQuery',
+              'Jest, Enzyme, Karma, Jasmine, Chai',
+              'CSS, LESS, SASS/SCSS',
+              'Node',
+              'Docker',
+              'Webpack',
+              'Yarn, NPM',
+              'Bootstrap 3 & 4, including SCSS integration',
+              'Photoshop',
+              'Symfony2',
+              'SOLID',
+              'Behaviour-driven development (BDD)',
+              'Gherkin scenario writing/refining',
+              'Test-driven development (TDD)',
+              'Git, SVN',
+            ]}/>
+            <Br/>
+
+            <Subtitle>Agile skills</Subtitle>
             <List points={[
               'Lean Kanban expert.',
               'Agile champion & coach.',
@@ -62,15 +85,16 @@ export default class App extends Component {
             <Br/>
 
             <Subtitle>Me</Subtitle>
-            <p>I'm a data-driven thinker and I have strong Agile analytical and organisational skills.</p>
-            <p>I attend various Kanban and Agile meetups in order to keep myself up to date with how other companies and coaches tackle real-world Lean delivery issues.
-              Although I do reference a variety of reading material, meetups are my go-to source for on-the-ground techniques and advice.</p>
+            <p>Front-end software has always been my core passion.</p>
+            <p>I'm a data-driven thinker and I use my strong Agile analytical and organisational skills to drive
+              technically excellent and sustainable solutions that my team and I build.</p>
             <ReadMore>
-              <p>I love the different unique challenges around software delivery. I understand that it is vital to become
-                invested in a product in order to deliver it to it's full potential.</p>
+              <p>I attend various Agile meetups in order to keep myself up to date with how other teams tackle
+                real-world Lean delivery issues. Although I do reference a variety of reading material, meetups are my
+                go-to source for on-the-ground techniques and advice.</p>
               <p>I am able to work on my own, but I have almost always found that teamwork trumps all silo-based work.</p>
               <p>I am lively, outgoing and approachable. I love playing video games (especially RPGs), developing software
-                on pet projects and cooking in my new dream kitchen, having wanted to be a chef when I was younger.</p>
+                on pet projects (currently learning Lua!) and cooking in my new dream kitchen, having wanted to be a chef when I was younger (before I got my hands on a PC!).</p>
               <p>I'm a fan of electronic house music and enjoy attending music festivals.</p>
               <p>My person-to-person and written skills are excellent and I always try to bring enthusiasm and adaptability to my work.</p>
             </ReadMore>
@@ -94,37 +118,28 @@ export default class App extends Component {
           <Col xs={12} lg={6}>
             <Subtitle>Experience</Subtitle>
             <Experience
-              jobTitle="Agile Delivery Manager"
+              jobTitle="Senior Front-End Software Engineer"
               companyName="BrightTALK"
               companyLogo={brighttalkLogo}
-              otherTitles={['Senior Front-End Software Engineer', 'Team Lead']}
+              otherTitles={['Agile Delivery Manager', 'Team Lead']}
               startDate={1301313600000}
               endDate={1530532800000}
             >
               <p>Responsible for leading the team working on the main client-facing Symfony2 and AngularJS website. My
-                role focused on being an Agile servant-leader for the team, moving from Scrum Master to Delivery
-                Manager as we transitioned away from the Scrum methodology towards Kanban.</p>
+                role focused on the development of our front-end as well as being an Agile servant-leader for the team.</p>
               <ReadMore>
-                <p>In the three year period of fully utilising an evolutionary MVP Lean Kanban approach, my team of
-                  five (2x developers, 1x tester, 1x product manager and myself) went from delivering between six and
-                  nine releases per year to thirty-seven, ninety-three and eighty-six releases in 2015, 2016 and 2017
-                  respectively. My team had no increase in staff members in this time - only Agile coaching, constant
-                  attention to technical excellence and the empowerment to evolve.</p>
-                <p>In the early stages, I would occasionally host Agile games in order to demonstrate the need for
-                  teamwork, breaking down work items, and how slowing down to speed up is a must. I also found this
-                  approach to be one of the best ways to coach others external to my team who were curious, but yet to
-                  be convinced.</p>
-                <TitledList title="Other responsibilities include:" points={[
-                  'Enable and coach the team to utilise Kanban to balance capacity against demand.',
+                <p>When the business changed direction away from their legacy products, I moved to a different team where I was required to plan the implementation of a completely new front-end stack, which included moving away from an old Backbone application with manual builds and no test coverage, to a React/Redux application (ES6) fully covered by Jest unit tests and automated Behaviour-driven scenarios, powered by Webpack, Yarn etc.</p>
+                <TitledList title="Agile responsibilities included:" points={[
+                  'Enable and coach the team to utilise Kanban to balance capacity against demand and prevent burnout.',
                   'Analyse team bottlenecks and adapt the team approach as required, using Little’s Law, queueing theory and the theory of constraints.',
-                  'Track team delivery cadence with cycle times and lead times to identify issues early and enable discussion and adaptation.',
+                  'Track team delivery cadence with cycle times and lead times to identify issues early.',
                   'Facilitate team ceremonies and reduce feedback loops with face-to-face conversations.',
-                  'Coach the team in the Responsibility process and encourage a safe-to-fail culture for developers to thrive with psychological safety.',
-                  'Protect the team from external interruption and unnecessary context switching',
+                  'Encourage a safe-to-fail culture for developers to thrive with psychological safety.',
+                  'Protect the team from external interruption and unnecessary context switching.',
                   'Measure the cost of delay to reveal undeniable need for change.',
-                  'Help the team define explicit policies for each stage of work in a team’s process.',
-                  'Radiate any and all information out in the open so the status of things is always evident to the team, stakeholders and business.',
-                  'Coach the team to focus and invest in customer needs as a priority, providing them the opportunity to help shape what a product looks like and offers.',
+                  "Help the team define explicit policies for each stage of work in a team's process.",
+                  'Radiate any and all information out in the open so the status of things is always evident to the team and stakeholders.',
+                  'Coach the team to focus on customer needs as a priority, providing them the opportunity to help shape what a product looks like and offers.',
                   'Coach the team to ship minimum viable products to reduce both time to market and cost of delay.',
                   'Work with business people to introduce Gherkin scenarios in order for teams, developers and the business to speak the same consistent language before, during and after each project.',
                 ]}/>
