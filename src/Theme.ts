97d53dd097b776b8983274829fd679dc1a0a833f
Dean James <dean.james6@tesco.com>
2020-12-10 17:52:47 +0000

    Start moving toward styled-components. Convert Br to styled-component. Add main theme file.

---
+++ src/Theme.ts
@@ -0,0 +1,22 @@
+//https://coolors.co/eae1df-3d405b-d52941-fcd581-e09f3e
+
+const colours = {
+  primary: "#3d405b",
+  secondary: "#d52941",
+  tertiary: "#e09f3e",
+  //$body-color: $primary
+};
+
+const baseFontSize = 16;
+const fontSizes = {
+  h1: baseFontSize * 2.5,
+  h2: baseFontSize * 1.6,
+  h3: baseFontSize * 1.4,
+  h4: baseFontSize * 1.25,
+  h5: baseFontSize * 1.25,
+  h6: baseFontSize,
+};
+
+const sectionSpacing = "25px";
+
+export { colours, sectionSpacing, fontSizes };
